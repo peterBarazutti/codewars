476e334afafefcb8a4b250807d75d51dac720536
peterBarazutti <peterBarazutti@users.noreply.github.com>
2018-10-04 20:01:38 +0200

    need to change how code works with repeating chars

--- src/main/java/three_kyu/Anagrams.java
+++ src/main/java/three_kyu/Anagrams.java
@@ -13,20 +13,24 @@ public class Anagrams {
                 calcFactorial(charList.size() - 1));
         Collections.sort(deduped);
 
-        BigInteger multiplier = BigInteger.valueOf(1 + deduped.indexOf(word.substring(0,1)));
+        BigInteger multiplier = BigInteger.valueOf(deduped.indexOf(word.substring(0,1)));
 
         BigInteger denominator = BigInteger.valueOf(1);
 
         for (String letter : deduped) {
+            List<String> tempList = new ArrayList<>(charList);
+            tempList.remove(letter);
             denominator.multiply(
                     BigInteger.valueOf(
-                            calcFactorial(Collections.frequency(charList, letter))));
+                            calcFactorial(Collections.frequency(tempList, letter))));
         }
 
         BigInteger variations = multiplier.multiply(nominator.divide(denominator));
         BigInteger leftover = BigInteger.ZERO;
         if (word.length() > 1) {
             leftover = listPosition(word.substring(1, word.length()));
+        } else {
+            return BigInteger.valueOf(1);
         }
 
         return variations.add(leftover);
